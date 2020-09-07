8e110771e6bf0de26a792451cd0a790e9883c993
Alex Seregin <alexeusgr@yahoo.com>
2020-09-07 16:09:05 +0300

    data cleaning: split the dataframe

--- eqty_ty.py
+++ eqty_ty.py
@@ -7,5 +7,16 @@ from pathlib import PosixPath
 PATH = '/home/alex/HDD/workshop/.datasets/KZnO/2.xls'
 
 # read the format properly
-df = pd.read_excel(PATH,verbose=True,sheet_name='данные')
+df = pd.read_excel(PATH,verbose=True,sheet_name='данные',
+                    usecols=(range(13)),
+                    header=0,
+                    na_values=[' ']).fillna(0)
+df.columns = (range(13))
+
+print(df.iloc[2::2,:].head())
+print(df.iloc[2::2,:])
+
+# split df into ranges:
+# print full 
+# find slices for sections.
 
