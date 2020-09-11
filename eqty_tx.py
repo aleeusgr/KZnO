308a2b1d57884f2410a535ffe785006f747a698c
Alex Seregin <alexeusgr@yahoo.com>
2020-09-11 17:10:08 +0300

    tx resampling, ty cleaning

--- eqty_tx.py
+++ eqty_tx.py
@@ -1,34 +1,39 @@
 #!bin/python
 
-def get_data():
-    """Load data from the web API
-    todo: fix date range 
-          """
-    import pandas_datareader as pdr
-    import pandas_datareader.data as web
-    import matplotlib.pyplot as plt
-    import datetime as dt
+import pandas_datareader.data as web
+import pandas as pd
+import matplotlib.pyplot as plt
+import datetime as dt
 
-    symbol = "CL=F"
-    provider = {
-    0:'yahoo',  # symbols: MSFT
-    1:'quandl', #has specific symbol format. symbols:??? 
-    2: 'moex',  #Moscow Exchange.  symbols:??? 
-    3: 'fred',  #???
-    4: 'stooq', #common index data. symbols: ^DJI
-    5: 'eurostat', #browse website for symbols
-    6: 'iex',   # requires API key
-    }
-    
-    start = '1999-01-01'
-    end = None
+symbol = "CL=F"
+provider = {
+0:'yahoo',  # symbols: MSFT
+1:'quandl', #has specific symbol format. symbols:??? 
+2: 'moex',  #Moscow Exchange.  symbols:??? 
+3: 'fred',  #???
+4: 'stooq', #common index data. symbols: ^DJI
+5: 'eurostat', #browse website for symbols
+6: 'iex',   # requires API key
+}
 
-    data = web.DataReader(symbol,provider[0],start,end) 
+start = '1998-01-01'
+end = None
+
+#data = web.DataReader(symbol,provider[0],start,end) 
+def fetch():
     oil = web.DataReader('CL=F',provider[0],start,end) 
     rub = web.DataReader('RUB=X',provider[0],start,end) 
     oil.to_csv('oil.csv')
     rub.to_csv('rub.csv')
- 
-# Procedure for transforming daily to monthly
+#fetch()
+
+oil = pd.read_csv('oil.csv')
+rub = pd.read_csv('rub.csv')
+
+df = rub
+df.set_index('Date', inplace=True)
+df.index = pd.to_datetime(df.index)
+df.resample('1M').mean()
+
 # Feature extraction ideas: Indicator from normalized world index values. provider[4]
 
--- eqty_ty.py
+++ eqty_ty.py
@@ -8,33 +8,30 @@ import numpy as np
 #PATH = PosixPath('/home/alex/HDD/workshop/.datasets/KZnO/1.xls') 
 PATH = '/home/alex/HDD/workshop/.datasets/KZnO/2.xls'
 
-
 df = pd.read_excel(PATH,sheet_name='данные',
                     usecols=(range(13)),
                     header=0,
                     na_values=[' ']).fillna(0)
 df.columns = (range(13))
 
-pd.options.display.max_rows = 100
-#print(df.iloc[2::2,:])
 
-vol =  df.iloc[50:96:2,:].copy().reset_index(drop=True)
-prft = df.iloc[2:47:2,:] .copy().reset_index(drop=True)
+volume = df.iloc[50:96:2,:].copy().reset_index(drop=True)
+profit = df.iloc[ 2:47:2,:].copy().reset_index(drop=True)
 
-##Visualise
-##
-plot = prft.iloc[0:,1:].to_numpy()
-plot = plot.astype(float)
+def visualise(df):
+    plot = df.iloc[0:,1:].to_numpy()
+    plot = plot.astype(float)
 
-import matplotlib.pyplot as plt
-import calendar
-plt.figure(figsize=(20,10))
-months = [calendar.month_abbr[i] for i in range(1,13)]
-plt.yticks(np.arange(12),labels=months)
-plt.xticks(np.arange(23),list(range(1998,2021)))
+    import matplotlib.pyplot as plt
+    import calendar
+    plt.figure(figsize=(20,10))
+    plt.yticks(np.arange(12),labels = [calendar.month_abbr[i] for i in range(1,13)])
+    plt.xticks(np.arange(23),list(range(1998,2021)))
 
-plt.imshow(plot.T)
-plt.show()
+    plt.imshow(plot.T)
+    plt.show()
 
 # transform into dataframe, parse dates
-df = pd.DataFrame()
+df = volume
+data = df.iloc[:,1:].to_numpy().reshape(276).astype(float)
+ 
