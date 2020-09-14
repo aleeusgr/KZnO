d946d6748a0d52d5281fafc1e03bec8add6190e8
Alex Seregin <alexeusgr@yahoo.com>
2020-09-15 01:28:21 +0300

    parse group users

--- sna.py
+++ sna.py
@@ -3,21 +3,33 @@
 import os
 import vk_api
 
-vk_session = vk_api.VkApi('+79202720942')
-vk_session.auth(token_only=True)
-vk = vk_session.get_api()
+def auth():
+    vk_session = vk_api.VkApi('+79202720942')
+    vk_session.auth(token_only=True)
+    return vk_session.get_api()
 
+vk = auth()
 
 friend_ids = vk.friends.get() #ids of active user friends
 #friends_verbose = vk.users.get(user_ids = friend_ids['items']) # get user info from ids
 #users_by_group = vk.groups.getMembers(group_id='zarya71') # get users of a group
+my_groups = vk.groups.get()['items']
+people_in_groups = {}
+for group_id in my_groups:
+    group = vk.groups.getById(group_ids=group_id)[0]['name']
+    people_in_groups[group] = vk.groups.getMembers(group_id=group_id)
+    print(group)
 
-#get users friends by username
-names = ('kanaby')
-user = vk.users.get(user_ids = names)
-user_id = user[0]['id']
-users_friends_ids = vk.friends.get(user_id = user_id)
-
+#def group_tested_methods(name = groups[0]):
+#    vk.groups.getById(group_ids=name)
+#    return vk.groups.getMembers(group_id=name)
+#
+#def user_tested_methods(names = ('kanaby')): # renam
+#    user = vk.users.get(user_ids = names)
+#    user_id = user[0]['id']
+#    return vk.groups.get(user_id=user_id) 
+#    #return  vk.friends.get(user_id = user_id)
+#
 # NOTES:
 # general: framework for VK data mining and exploration
 # module: generate a network of friends from list of users
