6a9f3162069b748c91a4271386b69d4b8eaf4d52
Alex Seregin <alexeusgr@yahoo.com>
2020-09-17 00:01:17 +0300

    scraping, saving data locally

--- sna.py
+++ sna.py
@@ -2,37 +2,30 @@
 
 import os
 import vk_api
+import networkx as nx
+import matplotlib.pyplot as plt
+import vk as v
+import pandas as pd
 
-def auth():
-    vk_session = vk_api.VkApi('+79202720942')
-    vk_session.auth(token_only=True)
-    return vk_session.get_api()
+vk = v.auth() #authentificate and instantiate
+# TODO:
+# graph visualisation: TACTICOOL-related groups, their users, bipartite graph. 
+# Data extraction task: consumer target group ID and social media presence. 
 
-vk = auth()
+#my_group_ids = vk.groups.get()['items']
+#usr_list = v.scrape_users_in_my_groups(my_groups)
+#dataset = v.scrape_groups()
 
-friend_ids = vk.friends.get() #ids of active user friends
-#friends_verbose = vk.users.get(user_ids = friend_ids['items']) # get user info from ids
-#users_by_group = vk.groups.getMembers(group_id='zarya71') # get users of a group
-my_groups = vk.groups.get()['items']
-people_in_groups = {}
-for group_id in my_groups:
-    group = vk.groups.getById(group_ids=group_id)[0]['name']
-    people_in_groups[group] = vk.groups.getMembers(group_id=group_id)
-    print(group)
+my_groups = v.load()
 
-#def group_tested_methods(name = groups[0]):
-#    vk.groups.getById(group_ids=name)
-#    return vk.groups.getMembers(group_id=name)
-#
-#def user_tested_methods(names = ('kanaby')): # renam
-#    user = vk.users.get(user_ids = names)
-#    user_id = user[0]['id']
-#    return vk.groups.get(user_id=user_id) 
-#    #return  vk.friends.get(user_id = user_id)
-#
-# NOTES:
-# general: framework for VK data mining and exploration
-# module: generate a network of friends from list of users
-# list of users can come from: users, groups, ???
-# 
-# 
+tactic = pd.DataFrame(my_groups).loc[[0,1,2,6,11,13,18,19,20,22,23]]
+
+
+#v.save(dataset)
+dataset = []
+
+G = nx.Graph()
+G.add_nodes_from(dataset)
+print(G.nodes)
+#nx.draw_circular(G)
+#plt.show()
