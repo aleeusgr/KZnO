84dc9eeb6c11248cf8f1f73531909af2211fdfad
Alex Seregin <alexeusgr@yahoo.com>
2020-10-04 12:26:07 +0300

    code review

--- eqty/dprep.py
+++ eqty/dprep.py
@@ -2,7 +2,7 @@
 ''' Procedures to transform the data:
 X: loaded automatically
 DO: functionality to choose provider and tickers. low priority
-Y: move to tx.py, rename tx.py to data.py? NO! keep modules small
+Y: index broken, what happened? cleaned old csv's?
 
 
 return (train_x,train_y,test_x,test_y)'''
@@ -18,23 +18,25 @@ import numpy as np
 
 dataX = tx.loadX(save_locally=True)
 dataY = ty.loadY() # np.array(23,12), Jan1998 : Dec2020
-#vis.plot_array(dataY,init_year=1998)
+#vis.plot_array(dataY,init_year=1998) init_year needs fixing
 # 
+# train_x and train_y date alignment:
 init_date = dt.date.fromisoformat(dataX.iloc[0,0]) # depends on choice of tickers
-train_end_date = dt.date.fromisoformat('2020-08-30')
+train_end_date = dt.date.fromisoformat('2020-08-30')# manuallys set from train_y original source
 xy_end_diff = train_end_date-dt.datetime.today().date()
 extra_months = xy_end_diff.days//30+1 
 
+# write tests to compare dates here and in the original source:
 train_x = dataX.iloc[:extra_months,1:].to_numpy() 
-train_y = dataY.reshape(-1)[-1+12*(init_date.year-1998)+init_date.month:-4] # FIX SHAPE!
-#import tensorflow as tf
+train_y = dataY.reshape(-1)[-1+extra_months+12*(init_date.year-1998)+init_date.month:-4] #this migth break
 
-df = dataX.iloc[:-1,:].copy()
+# Scatterplots:
+df = dataX.iloc[:extra_months,:].copy()
 df['Y'] = train_y.tolist()
 df['Y$'] = df['Y'] / df['rub']
 #im = plt.matshow(df.corr(),interpolation='none', vmin=0, vmax=1, aspect='equal')
 
-
+# There was an example of this in one of the tutorials, check HandsOn ML
 import itertools
 l = list(itertools.combinations(df.drop('Date',axis=1).columns, 2))
 for i in l:
